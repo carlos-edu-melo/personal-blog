72d557a504c9d91fc4acc364353d375670d28840
Carlos Melo <carlos.melo@unisuam.edu.br>
2022-08-24 18:01:37 -0300

    🚀

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './style.css'
+import { GenerateMenus } from './components/list-of-menus'
+import { DateNow } from './components/subheader'
+
+GenerateMenus.generateMenuByString()
+DateNow.showInfoAboutDate()
