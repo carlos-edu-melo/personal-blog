6babcc1f17a6c4380148ce28e848e51ba2f7263e
carlosmelo <carlos.melo>
2022-09-01 22:13:04 -0300

    fix bug in the date

--- src/components/date.ts
+++ src/components/date.ts
@@ -3,18 +3,18 @@ let date = new Date;
 const $dateInfo = document.querySelector('.dateInfo') as HTMLElement
 
 export const InformationAboutDate = {
-  day: date.getUTCDate(),
-  mouth: date.getUTCMonth() + 1,
-  year: date.getUTCFullYear(),
+  day: date.getDate(),
+  mouth: date.getMonth() + 1,
+  year: date.getFullYear(),
   hours: date.getHours(),
 
   showInfoAboutDate: function (): string {
     let dateFormat: string = `${this.day}/${this.mouth}/${this.year} `
 
-    if (this.hours >= 5 && this.hours <= 12) {
+    if (this.hours > 5 && this.hours < 12) {
       $dateInfo.textContent = `Bom Dia! Hoje é dia ${dateFormat}`
     }
-    if (this.hours >= 13 && this.hours <= 17) {
+    else if (this.hours > 13 && this.hours < 17) {
       $dateInfo.textContent = `Boa Tarde! Hoje é dia ${dateFormat}`
     } else {
       $dateInfo.textContent = `Boa Noite! Hoje é dia ${dateFormat}`
